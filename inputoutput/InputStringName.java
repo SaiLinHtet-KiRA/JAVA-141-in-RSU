6850bf1504b233c33833c37a7f1a48ab91bca748
6807665 <6807665@ITSC.LOCAL>
2025-08-27 14:57:41 +0700

    week-2

---
+++ inputoutput/InputStringName.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class InputStringName {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name="";
+        System.out.print("Please input your name:");
+        name=sc.nextLine();
+        System.out.print("Hi, Nice too meet you : "+name);
+    }
+}
---
+++ inputoutput/Interview.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Interview {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name="",career="",addressString="",hobby="",studyMajor="";
+       
+        System.out.print("Please input your name: ");
+        name=sc.nextLine();
+
+        System.out.print("Please input your career: ");
+        career=sc.nextLine();
+
+        System.out.print("Please input your number of friends: ");
+       int friends=sc.nextInt();
+        System.out.print("Please input your age: ");
+        int age=sc.nextInt();
+        
+        System.out.print("Please input your addressString: ");
+        addressString=sc.nextLine();
+
+        System.out.print("Please input your hobby: ");
+        hobby=sc.nextLine();
+
+        System.out.print("Please input your studing major: ");
+        studyMajor=sc.nextLine();
+
+        System.out.println("Hi, Nice to meet you : "+name);
+        System.out.println("Your career : "+career);
+        System.out.println("Your friends : "+friends);
+        System.out.println("Your age : "+age);
+        System.out.println("Your addressString: "+addressString);
+        System.out.println("Your hobby : "+hobby);
+        System.out.println("Your studyMajor : "+studyMajor);
+
+        
+
+       
+
+    }
+}

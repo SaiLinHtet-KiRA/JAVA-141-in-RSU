d08e04d12a0d363c8e88930e00233bc8c6d53237
6807665 <6807665@ITSC.LOCAL>
2025-08-27 15:55:27 +0700

    week 2

---
+++ inputoutput/ComputeSqrtandPowerofIntValue.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ComputeSqrtandPowerofIntValue {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please input integer n : ");
+        int number = sc.nextInt();
+
+        double Sqrt = Math.sqrt(number);
+        int power = number * number;
+
+        System.out.print("So Sqrt of n = " + Sqrt + " and power of n =" + power);
+
+    }
+}
---
+++ inputoutput/ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConvertCelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is celsius degree: ");
+        int celsius = sc.nextInt();
+
+        double fahrenheit = (double) (celsius * 9.0 / 5.0) + 32;
+        System.out.print("So temperature in fahrenheit is : " + fahrenheit);
+
+    }
+}

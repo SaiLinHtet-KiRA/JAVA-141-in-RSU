274b3c7dc41a5c53e6c4fa39364bc671646a9162
6807665 <6807665@ITSC.LOCAL>
2025-08-27 13:47:27 +0700

    week-1

---
+++ inputoutput/PraticProgram3.java
@@ -0,0 +1,10 @@
+public class PraticProgram3 {
+    public static void main(String[] args) {
+      System.out.println("\ta\tb\tc");
+      System.out.println("\\\\");
+      System.out.println("'");
+      System.out.println("\"\"\"");
+      System.out.println("C:\nin\the download spiral");
+
+    }
+  }

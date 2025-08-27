8e1ad33738fedfc02044721783b9e1fce7f0f265
6807665 <6807665@ITSC.LOCAL>
2025-08-27 15:30:22 +0700

    week-2

--- inputoutput/ComputeAreaAndCircle.java
+++ inputoutput/ComputeAreaAndCircle.java
@@ -4,5 +4,13 @@ public class ComputeAreaAndCircle {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Input integer of radiant : ");
+        int radiant = sc.nextInt();
+
+        double area = (double) Math.PI * (radiant * radiant);
+        double circle = (double) 2 * Math.PI * radiant;
+
+        System.out.print("So area of radiant is : " + area + " and circle =" + circle);
+
     }
 }

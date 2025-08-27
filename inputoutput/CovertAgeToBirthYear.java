d9db9433ba3afb7efe8fb7471bce530d786f7746
6807665 <6807665@ITSC.LOCAL>
2025-08-27 15:10:15 +0700

    week2

---
+++ inputoutput/CovertAgeToBirthYear.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CovertAgeToBirthYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is your age: ");
+        int age = sc.nextInt();
+        int birthYear = 2025 - age;
+
+        System.out.print("So your birth year is " + birthYear);
+    }
+}

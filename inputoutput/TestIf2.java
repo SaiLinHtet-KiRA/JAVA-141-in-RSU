d858f1100f5c3d43324640b747b518a0e62c24ba
SaiLinHtet-KiRA <kira.official.dev@gmail.com>
2025-09-03 13:48:08 +0700

    week-3

---
+++ inputoutput/TestIf2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestIf2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("input student grade: ");
+
+        int studentGrade = sc.nextInt();
+
+        if (studentGrade >= 60) {
+            System.out.print("Pass !!");
+        } else {
+            System.out.print("Failed");
+
+        }
+    }
+}

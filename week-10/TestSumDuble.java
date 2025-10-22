35be6584f1101e97ff5060285354d105b9aa1120
Sai Lin Htet <sailinhtet.d@gmail.com>
2025-10-22 13:16:35 +0700

    update

---
+++ week-10/TestSumDuble.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class TestSumDouble{
+    public static void main(String[] args){
+          Scanner sc= new Scanner(System.in);
+          System.out.print("input number of double  value :"); 
+          int n =0; double sum =0;
+         n = sc.nextInt();
+         for(int i =1; i<=n; i++){
+             System.out.print("Input double value : "); 
+             sum+= sc.nextDouble();
+          }
+            System.out.print("Average = " + (sum/n)); 
+ }       
+}
